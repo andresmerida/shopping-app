0f2150384892067ddbab89ec766597c667cb92af
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2024-06-04 17:04:03 -0400

    adding openAPI

--- order-service/src/main/java/org/adk/shopping/order/client/InventoryClient.java
+++ order-service/src/main/java/org/adk/shopping/order/client/InventoryClient.java
@@ -1,12 +1,9 @@
 package org.adk.shopping.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventories/{skuCode}/{quantity}/available")
+    @GetExchange("/api/inventories/{skuCode}/{quantity}/available")
     boolean isInStock(@PathVariable String skuCode, @PathVariable Integer quantity);
 }
